6bd1e8ef96c79068014aeddf4618f4c021b04136
Leo Gutierrez <149627604+LeoGtz356@users.noreply.github.com>
2023-11-01 16:38:06 -0500

    Update CAS-TESTING CODE.ino

--- CAS-TESTING CODE.ino
+++ CAS-TESTING CODE.ino
@@ -7,6 +7,9 @@ int buttonState = 0;
 
 bool isOn = false;
 
+//Crazy? I was crazy once. They locked me in a room. A rubber room! 
+//A rubber room with rats,and rats make me crazy. 
+//Crazy? I was crazy once. They locked me in a ro- 
 
 void setup() {
 
