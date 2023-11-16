4ee40923061f9a20221f9c6f658f0e96f552d3d9
JJ-UT <149627462+JJ-UT@users.noreply.github.com>
2023-11-16 17:33:10 -0600

    every event coded

--- CAS_MC_MAIN.ino
+++ CAS_MC_MAIN.ino
@@ -5,11 +5,10 @@ const int ledPin = 9;
 /// Buttons///////////
 
 const int button1 = 2;
-const int button2 = 0;
-const int button3 = 3;
-const int button4 = 4;
-const int button5 = 5;
-
+const int button2 = 3;
+const int button3 = 4;
+const int button4 = 5;
+const int button5 = 12;
 
 
 
@@ -58,6 +57,7 @@ Servo servoE4;
 Servo servoE5;
 
 int pos;
+int pos2;
 
 
 
@@ -85,14 +85,23 @@ void setup() {
   servoE1.attach(6);   /// SERVO PIN
   servoE2.attach(7);   /// SERVO PIN
   servoE3.attach(8);   /// SERVO PIN
-  servoE4.attach(9);   /// SERVO PIN
-  servoE5.attach(10);  /// SERVO PIN
+  servoE4.attach(11);   /// SERVO PIN
+  servoE5.attach(13);  /// SERVO PIN
 }
 
 
 void loop() {
   buttonState1 = digitalRead(button1);
+  buttonState2 = digitalRead(button2);
+  buttonState3 = digitalRead(button3);
+  buttonState4 = digitalRead(button4);
+  buttonState5 = digitalRead(button5);
+
+
+
+
   //Serial.println(angle);
+
 
   ///// EVENT 1//////////////////////////
   //////////////////////////////////////
@@ -113,80 +122,98 @@ void loop() {
 
     for (pos = 0; pos <= 36.5; pos = pos + 1.5) {
       servoE1.write(pos);
-      delay(servDelay);
+
+
     }
 
     for (pos = 36.5; pos >= 0; pos = pos - 1.5) {
       servoE1.write(pos);
-      delay(servDelay);
+
     }
 
   } else {
     //OFF
     servoE1.write(0);
-    Serial.println("OFF");
+    //Serial.println("button 1OFF");
+
+
+
   }
 
 
   /////////////////////////////
   ///////// Event 2////////////
-  if (event2 == true) {
-    buttonState2 = digitalRead(button2);
+ //if (event2 == true) {
     if (buttonState2 != prevButtonState2) {
       if (buttonState2 == HIGH) {
         Serial.println("button 2 on ");
         isOn2 = !isOn2;
       }
-    }
+      if (isOn2 == true) {
+      //event3 = true;
+        
+        servoE2.write(90);
 
-    if (isOn2 == true) {
-      event3 = true;
+          
+
       
 
 
 
     } else {
       //OFF
+       servoE2.write(0);
+          Serial.println("button 2 off ");
+
+
     }
-  }
+   }
+  // }
 
   //// Event 3//////////////////
   //////////////////////////////
 
-  if (event3 == true) {
-    buttonState3 = digitalRead(button3);
+  //if (event3 == true) {
     if (buttonState3 != prevButtonState3) {
       if (buttonState3 == HIGH) {
         Serial.println("Button 3 on");
         isOn3 = !isOn3;
       }
-    }
+       if (isOn3 == true) {  //ON
+      //event4 = true;
+      
+                servoE3.write(90);
 
-    if (isOn3 == true) {  //ON
-      event4 = true;
+
+
 
 
 
 
     } else {
       //OFF
+             servoE3.write(0);
+
+      Serial.println("Button 3 OFF");
     }
   }
+//}
 
   /////////////// Event 4///////////////////////
   //////////////////////////////////////////////
 
-  if (event4 == true) {
-    buttonState4 = digitalRead(button4);
+  //if (event4 == true) {
     if (buttonState4 != prevButtonState4) {
       if (buttonState4 == HIGH) {
         Serial.println("Button 4 on");
         isOn4 = !isOn4;
       }
-    }
-
-    if (isOn4 == true) {  //ON
+       if (isOn4 == true) {  //ON
       event5 = true;
+
+        servoE4.write(60);
+
+      
 
 
 
@@ -194,29 +221,42 @@ void loop() {
 
     } else {
       //OFF
+        Serial.println("Button 4 OFF");
+        servoE4.write(0);
+
+
     }
-  }
+    }
+//}
 
   ////////////// EVENT 5////////////////////
   //////////////////////////////////////////
-  if (event5 == true) {
-    buttonState5 = digitalRead(button5);
+  //if (event5 == true) {
     if (buttonState5 != prevButtonState5) {
       if (buttonState5 == HIGH) {
-        Serial.println("Button 3 on");
+        Serial.println("Button 5 on");
         isOn5 = !isOn5;
       }
     }
 
     if (isOn5 == true) {  //ON
+              servoE5.write(30);
 
 
 
 
     } else {
-      //OFF
+       //OFF
+
+        Serial.println("Button 5  OFF");
+
+
+
+              servoE5.write(0);
+
+
     }
-  }
+  //}
 
 
 
